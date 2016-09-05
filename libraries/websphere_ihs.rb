bc9f11955c5e212bfff9580bc790e3bd78d6d4e4
lmunro <lachlan.munro@gmail.com>
2016-09-05 16:37:08 +0100

    ihs minor change with comments

--- libraries/websphere_ihs.rb
+++ libraries/websphere_ihs.rb
@@ -9,11 +9,11 @@ module WebsphereCookbook
     property :wct_root, String, default: '/opt/IBM/WebSphere/Toolbox'
     property :wct_tool_root, String, default: lazy { "#{wct_root}/WCT" }
     property :config_type, String, default: 'local_distributed', regex: /^(local_distributed|remote)$/ # currently only tested for local_distributed
-    property :profile_name, String, required: true
+    property :profile_name, String, required: false # only valid for local_distributed installations
     property :node_name, String, required: true
     property :profile_path, String, default: lazy { "#{websphere_root}/profiles/#{profile_name}" }
     property :map_to_apps, [TrueClass, FalseClass], default: false
-    property :ihs_hostname, [String],  default: lazy { node.name }
+    property :ihs_hostname, [String],  default: lazy { node.hostname }
     property :ihs_port, String, default: '80'
     property :ihs_install_root, default: '/opt/IBM/HTTPServer'
     property :ihs_config_file, String, default: lazy { "#{ihs_install_root}/conf/httpd.conf" }
@@ -25,6 +25,7 @@ module WebsphereCookbook
     property :ihs_admin_password, [String, nil], default: nil
     property :ihs_admin_port, String, default: '8008'
 
+    # pre-requisite: this library only supports ih webservers on managed nodes. The websphere node must already be created on the ihs node.
     # 1. install required packages for wctcmd tool on centos 6
     # 2. install web definition by running wctcmd pct tool
     # 3. execute equivalient to contents of configure script file
@@ -88,7 +89,7 @@ module WebsphereCookbook
         mycmd = Mixlib::ShellOut.new('./wctcmd.sh -tool pct -listDefinitionLocations', cwd: wct_tool_root)
         mycmd.run_command
         if mycmd.error?
-          Chef::Log.warn('Error checking if web definition exists.')
+          Chef::Log.warn("Error checking if web definition exists: #{mycmd.stderr}")
           return false
         end
         if mycmd.stdout.include?('Path:')
@@ -99,9 +100,33 @@ module WebsphereCookbook
         false
       end
 
+      # This method runs configureWebserverDefinition.jacl script passing in paramaters.
+      # wsadmin.sh -user WASadminUsername -password WASadminPassword -f ${WAS_HOME}/bin/configureWebserverDefinition.jacl webserverName webserverType webserverInstallLocation webserverConfigFile webserverPort
+      #   mapApplications pluginInstallLocation webserverNodeType webserverNodeName webserverHostName webserverOS adminPort adminUserID adminPassword
+      # Details of each attribute:
+      # WASadminUsername         : User Name for WAS Adminstration
+      # WASadminPassword         : Password  for WAS Adminstration
+      # webserverName            : Name of the webserver to be defined
+      # webserverType            : Type of the web server. Valid values are: IHS APACHE IPLANET DOMINO IIS HTTPSERVER_ZOS
+      # webserverInstallLocation : Location of the websserver install
+      # webserverConfigFile      : Webserver configuration file
+      # webserverPort            : Listening port of the web server
+      # mapApplications          : Specifies whether the existing applications are to be mapped to the web server. Valid values are: MAP_NONE MAP_DEFAULT MAP_ALL
+      # pluginInstallLocation    : Location of the plugin install
+      # webserverNodeType        : Type of the node holding webserver definition, valid values are: managed unmanaged
+      # webserverNodeName        : Node at which web server should be defined
+      # webserverHostName        : Host name of the web server Needed only for unmanaged node
+      # webserverOS              : Operating system of the web server machine Needed only for unmanaged node Valid values are: windows solaris aix hpux linux os400 os390
+      # adminPort                : IHS Remote Administration port Needed only for IHS webserver Defaults to 2001 for os400 Defaults to 8008 for other platforms
+      # adminUserID              : IHS Remote Administration userid Needed only for IHS webserver
+      # adminPassword            : IHS Remote Administration password Needed only for IHS webserver
+      # serviceName              : Service name for the webserver Needed only for IHS webserver
       def run_configure_script
         # run configure script on dmgr
-        # TODO: ensure this works when webserver is not on same server as dmgr
+
+        # /opt/IBM/WebSphere/AppServer/profiles/A1_profile/bin/wsadmin.sh -profileName A1_profile -user idadmin -password 99Pr0blems
+        # -f '/opt/IBM/WebSphere/AppServer/bin/configureWebserverDefinition.jacl' Webserver01 IHS '/opt/IBM/HTTPServer' '/opt/IBM/HTTPServer/servers/Webserver01/conf/httpd-Webserver01.conf' 1143 MAP_ALL
+        # '/opt/IBM/WebSphere/Plugins' managed ip-10-1-1-191_Node01 ibm-ihs-managed-centos-6 linux
 
         configure_cmd = "#{profile_path}/bin/wsadmin.sh -profileName #{profile_name} "\
           "-user #{admin_user} -password #{admin_password} -f '#{bin_dir}/configureWebserverDefinition.jacl' "\
