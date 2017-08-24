7d339c411536bd5054830070d6640bff21f5b9d9
Chris Minton <christopher.minton@sainsburys.co.uk>
2017-08-24 15:07:11 +0100

    Add update action to ibm_cert
    - Add an update action to ibm_cert to allow updates of existing labels within a particular keystore
    - Test whether, if a certificate label exists in a keystore, the importing certificate matches the fingerprint of the current

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'lachlan.munro@sainsburys.co.uk'
 license 'Apache 2.0'
 description 'Installs/Configures IBM Websphere'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.0.4'
+version '1.0.5'
 supports 'redhat'
 supports 'centos'
 
